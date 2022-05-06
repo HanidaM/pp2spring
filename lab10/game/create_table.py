d9630bf460749a5f49fe516b295ea93ddfaec8ea
kilybay_bauyrzhan <baurzhkk@gmail.com>
2022-05-06 13:35:56 +0600

    2nd updatw

--- lab10/game/create_table.py
+++ lab10/game/create_table.py
@@ -5,17 +5,14 @@ config = psycopg2.connect(**params)
 current = config.cursor()
 
 create_table = '''
-    CREATE TABLE PhoneBook(
-        name VARCHAR(255) ,
-        number VARCHAR(255) UNIQUE
+    CREATE TABLE records(
+        name VARCHAR(255) NOT NULL,
+        record INT NOT NULL
     );
 '''
 
 current.execute(create_table)
-# delete_raw = '''
-#     DELETE FROM PhoneBook WHERE name = %s;
-# '''
-# current.execute(delete_raw,('Eer',))
+
 current.close()
 config.commit()
 config.close()
---
+++ lab11/5.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host = 'localhost',
+    database = 'postgres',
+    port= 5432,
+    user = 'postgres',
+    password = 'HB@uka2003'
+)
+cur = conn.cursor()
+
+
+del_value = input('Enter name you want delete...\n')
+
+
+# procedure was created inside pg admin/tools/query tool
+'''
+create or replace procedure delete_user(del_value text)
+as
+$$
+begin
+    delete
+    from phonebook
+    where name = $1;
+end;
+$$
+    LANGUAGE plpgsql;
+
+'''
+cur.execute(f'CALL delete_user(\'{del_value}\');')
+
+
+cur.close()
+conn.commit()
+conn.close()
